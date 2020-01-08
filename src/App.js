ee26ad1fa945c494a2d833bc6773905b633f8f10
Fernanda Sampaio <fernanda@MacBook-Air-de-Fernanda.local>
2020-01-08 10:10:27 -0300

    reacttodoapp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lista: []
+    }
+  };
+
+  evento(e) {
+    if (e.keyCode === 13 && e.target.value !== '') {
+      this.setState({
+        lista: this.state.lista.concat(e.target.value)
+      });
+      e.target.value="";
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className= "text-center" >TO DO LIST</h1>
+            <input type="text" className="form-control" placeholder="Ingrese Tarea" onKeyDown={e => this.evento(e)} />
+            <br />
+            <ul className="list-group">
+              {
+                this.state.lista.length > 0 &&
+                this.state.lista.map((lista, i) => {
+                  return (
+                    <li key={i} className="list-group-item- d-flex justify-content-between">
+                      {lista}
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+} export default App;
